26cca0b818d99d1b4c0568891d7b6b32a8d7e8bb
Professor Retan <professorretan@gmail.com>
2017-03-08 21:37:18 -0800

    Set up player and enemy spawning as well as clean up on level end, start on player state

--- Assets/Code/Game/Characters/Player/Player.cs
+++ Assets/Code/Game/Characters/Player/Player.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum PlayerState
+{
+    PLAYER_SPAWNING,
+    PLAYER_IDLE,
+    PLAYER_MOVING,
+    PLAYER_ATTACKING,
+    PLAYER_HIT,
+    PLAYER_DYING,
+    PLAYER_DEAD
+}
+
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // The player's current state
+    PlayerState CurrentState = PlayerState.PLAYER_IDLE;
+
+    // Update is called once per frame
+    void Update() {
+        // Player STATE MACHINE
+        switch (CurrentState)
+        {
+            case PlayerState.PLAYER_IDLE:
+                break;
+
+        }
+    }
 }
